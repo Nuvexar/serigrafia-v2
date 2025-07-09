764ed5f4a6ab9df85fd741ce021728d23fd3875e
Felix Kevin Acarapi Rivero <nuvexar@gmail.com>
2025-07-09 18:04:41 -0300

    Configuraciones terminadas

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.js"        
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#121212",
+        accent: "#FF4081",
+        secondary: "#3D5AFE",
+        neutral: "#2A2A2A",
+        light: "#F5F5F5",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
